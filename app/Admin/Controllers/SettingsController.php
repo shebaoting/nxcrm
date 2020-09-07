0e27955dcec0ca6aa14bc5e02cc75ba8113799b3
shebaoting <th9th@th9th.com>
2020-09-07 10:51:06 +0800

    版本号

--- app/Admin/Controllers/SettingsController.php
+++ app/Admin/Controllers/SettingsController.php
@@ -14,7 +14,7 @@ class SettingsController extends Controller
      *
      * @var string
      */
-    const VERSION = '1.9.3';
+    const VERSION = '1.9.7';
 
     public function index(Content $content)
     {
