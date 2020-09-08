c2917e4a6e65777786561f5841770b02f678874f
牛铁柱 <th9th@th9th.com>
2020-09-08 13:26:45 +0800

    个人发票不需要填写识别号

---
+++ database/migrations/2020_09_08_132153_edit_invoice_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class EditInvoiceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('invoices', function (Blueprint $table) {
+            $table->string('tin')->default('')->nullable()->change();
+            $table->string('bank_name')->default('')->nullable()->change();
+            $table->string('bank_account')->default('')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
