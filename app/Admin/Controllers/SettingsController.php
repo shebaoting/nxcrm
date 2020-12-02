2515e628041007ed3abe30a0e36bc6820a196ab0
牛铁柱 <th9th@th9th.com>
2020-12-02 15:29:01 +0800

    跟进发布与自定义字段添加的问题修复

--- app/Admin/Controllers/SettingsController.php
+++ app/Admin/Controllers/SettingsController.php
@@ -14,7 +14,7 @@ class SettingsController extends Controller
      *
      * @var string
      */
-    const VERSION = '1.12.2';
+    const VERSION = '1.12.2.2';
 
     public function index(Content $content)
     {
