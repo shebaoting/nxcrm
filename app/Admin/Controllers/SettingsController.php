fd2e646dc045b9d796dc90f780bd50d26520369a
牛铁柱 <th9th@th9th.com>
2020-12-15 20:14:51 +0800

    增加客户分享功能

--- app/Admin/Controllers/SettingsController.php
+++ app/Admin/Controllers/SettingsController.php
@@ -14,7 +14,7 @@ class SettingsController extends Controller
      *
      * @var string
      */
-    const VERSION = '1.12.8';
+    const VERSION = '1.12.15';
 
     public function index(Content $content)
     {
