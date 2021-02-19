6f706f687e93d1ce78fad9d41b3741cfe54f8519
shebaoting <th9th@th9th.com>
2021-02-19 09:37:35 +0800

    修复了路由别名不正确的问题

--- resources/views/admin/contract/_top.blade.php
+++ resources/views/admin/contract/_top.blade.php
@@ -1,6 +1,6 @@
 <h1>{{$contract['title']}}<span>（#{{$contract['id']}}）</span></h1>
 <div class="row" style="margin-top: 20px; color:#666">
-    <div class="col-md-4 col-sm-3 col-12"><i class="feather icon-package"></i>所属客户：{{$customer['name']}}</div>
+    <div class="col-md-4 col-sm-3 col-12"><i class="feather icon-package"></i>客户：{{$customer['name']}}</div>
     @if ($contract['fields'])
     @php
     $contract_fields = json_decode($contract['fields'],true);
