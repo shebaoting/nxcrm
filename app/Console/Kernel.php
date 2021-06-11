48471849523beb6f6616ba0d43994941947fcdfa
shebaoting <th9th@th9th.com>
2021-06-11 12:40:19 +0800

    修复公海的命令行

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('nxcrm:high-seas')->daily();
+        $schedule->command('nxos:high-seas')->daily();
     }
 
     /**
