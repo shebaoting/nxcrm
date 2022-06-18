639024ac44c4c7d1c520a6f8192ddc26d8052cf4
牛铁柱 <th9th@th9th.com>
2022-06-18 16:34:28 +0800

    初始化仓库

--- resources/views/admin/customer/_operation_buttons.blade.php
+++
@@ -1,4 +0,0 @@
-<a href="{{ admin_route('customers.edit',['customer'=>$customer['id']])}}"
-    class="btn btn-light me-3" data-bs-toggle="modal">编辑</a>
-<a href="{{ admin_route('customers.destroy',['customer'=>$customer['id']])}}"
-    class="btn btn-primary">删除</a>
