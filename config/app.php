b093b66bfc884357d5fe1ed5e3a34f7e2d0a6cc0
牛铁柱 <th9th@th9th.com>
2022-06-20 17:06:52 +0800

    升级到l9

--- config/app.php
+++ config/app.php
@@ -175,7 +175,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\MyConfigServiceProvider::class,
+        // App\Providers\MyConfigServiceProvider::class,
     ],
 
     /*
